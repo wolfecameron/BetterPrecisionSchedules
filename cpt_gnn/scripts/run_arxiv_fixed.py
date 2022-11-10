d439559fd52865a25856ac1970d56afcc6084cda
Cameron Wolfe <wolfe.cameron@utexas.edu>
2022-11-10 19:39:19 +0000

    update fixed quantization script

--- cpt_gnn/scripts/run_arxiv_fixed.py
+++ cpt_gnn/scripts/run_arxiv_fixed.py
@@ -2,7 +2,7 @@
 
 import os
 
-gpu = 0
+gpu = 1
 epochs = 500
 dpt = 0.5
 lr_list = [1e-3]
@@ -15,18 +15,25 @@ eval_len = 25
 qsched = 'fixed'
 nbs = '8 8'
 ngbs = '8 8'
-trials = 1
-tags = 'axiv_static_quant1'
+trials = 2
+qnorms = [True, True]
+qaggs = [False, True]
+tags = 'axiv_static_arch_test'
 
-for lr in lr_list:
-    for lrs in lrs_list:
-        for trial in range(trials):
-            exp_name = f'ogbarxiv_static_{lr}_{lrs}_{trial}_smallmod_highdpt'
-            command = (
-                f'CUDA_VISIBLE_DEVICES={gpu} python train_arxiv.py --exp-name {exp_name} '
-                f'--n-layers {layers} --n-hidden {hid} --dropout {dpt} --lr {lr} --lr-schedule {lrs} '
-                f'--n-epochs {epochs} --weight-decay {wd} --eval_every {eval_len} '
-                f'--precision_schedule {qsched} --cyclic_num_bits_schedule {nbs} '
-                f'--cyclic_num_grad_bits_schedule {ngbs} --tags {tags} --use-wandb '
-            )
-            os.system(command)
+for qnorm, qagg in zip(qnorms, qaggs):
+    for lr in lr_list:
+        for lrs in lrs_list:
+            for trial in range(trials):
+                exp_name = f'ogbarxiv_static_{lr}_{lrs}_qnorm{qnorm}_qagg{qagg}_{trial}'
+                command = (
+                    f'CUDA_VISIBLE_DEVICES={gpu} python train_arxiv.py --exp-name {exp_name} '
+                    f'--n-layers {layers} --n-hidden {hid} --dropout {dpt} --lr {lr} --lr-schedule {lrs} '
+                    f'--n-epochs {epochs} --weight-decay {wd} --eval_every {eval_len} '
+                    f'--precision_schedule {qsched} --cyclic_num_bits_schedule {nbs} '
+                    f'--cyclic_num_grad_bits_schedule {ngbs} --tags {tags} --use-wandb '
+                )
+                if qnorm:
+                    command += f'--quant-norm '
+                if qagg:
+                    command += f'--quant-agg '
+                os.system(command)
