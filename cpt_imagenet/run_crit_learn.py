c1a79daf7cc726e0c5ce6eb7693cd8593a2d8fd9
Cameron Wolfe <wolfe.cameron@utexas.edu>
2022-12-30 10:44:21 -0600

    update CLP imagenet tests

--- cpt_imagenet/run_crit_learn.py
+++ cpt_imagenet/run_crit_learn.py
@@ -2,22 +2,21 @@ import os
 
 # test one
 # setup
-gpu = 1
+gpu = 0
 datadir = '/home/exx/data/imagenet/'
 base_save_dir = './quant_results'
-eval_every = 1
 
 # training
-def_epoch = [0, 200, 50, 100]
-total_epoch = [x + 200 for x in def_epoch]
-bs = 512
+def_epoch = [0, 100, 25]
+total_epoch = [x + 150 for x in def_epoch]
+bs = 256
+workers = 8
 lr_sched = 'piecewise'
-lr = 0.2
+lr = 0.1
 step_ratio = 0.1
 wd = 0.
-warm_up = False
 pretrained = False
-use_wandb = True
+use_wandb = False
 tags = 'imagenet_crit_learn'
 
 # quant
@@ -32,14 +31,14 @@ arch = f'resnet18'
 
 # stuff that changes
 for it, dit in zip(total_epoch, def_epoch):
-    exp_name = f'critlearn_{ds}_{arch}_{lr_sched}_quant{min_bit}_{dit}_00'
+    exp_name = f'critlearn_{ds}_{arch}_{lr_sched}_quant{min_bit}_{dit}_02'
     save_dir = os.path.join(base_save_dir, exp_name)
     if not os.path.exists(save_dir):
         os.mkdir(save_dir)
-    command = (f'CUDA_VISIBLE_DEVICES={gpu} python crit_learn.py --exp-name {exp_name} --cmd train '
+    command = (f'CUDA_VISIBLE_DEVICES={gpu} python crit_learn.py --exp-name {exp_name} '
             f'--arch {arch} --dataset {ds} --datadir {datadir} --epoch {it} --def-epochs {dit} '
             f'--batch_size {bs} --lr_schedule {lr_sched} --lr {lr} --weight_decay {wd} '
-            f'--step_ratio {step_ratio}  --save_folder {save_dir} --eval_every {eval_every} '
+            f'--step_ratio {step_ratio}  --save_folder {save_dir} --workers {workers} '
             f'--cyclic_num_bits_schedule {num_bits} --cyclic_num_grad_bits_schedule {num_grad_bit} '
     )
     if use_wandb:
